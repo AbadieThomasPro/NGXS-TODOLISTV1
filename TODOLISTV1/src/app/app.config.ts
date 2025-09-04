27d49e99bf6c24e13be32046f532eea3a95a52f3
Thomas ABADIE <t.abadie@cargo-services.fr>
2025-09-04 09:12:36 +0200

    version fonctionnel ajout d'item dans list

--- TODOLISTV1/src/app/app.config.ts
+++ TODOLISTV1/src/app/app.config.ts
@@ -2,16 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore, NgxsModule } from '@ngxs/store';
+import { NgxsModule } from '@ngxs/store';
 import { TodoState } from './store/todo.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideStore(
-      [],
-    ),
+    provideRouter(routes),
     importProvidersFrom(
       NgxsModule.forRoot([TodoState])
     )
